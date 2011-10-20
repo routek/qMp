76191e534aad2be4aac46af21d4125c1ac1d8089
Victor Oncins <victor@freakando.net>
2011-10-21 00:25:33 +0200

    Added some missing text lines in licence headers

--- packages/qmp-small-node/files/etc/qmp/qmp_common.sh
+++ packages/qmp-small-node/files/etc/qmp/qmp_common.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-# 
-# Copyright (C) 2011 Fundació Privada per a la Xarxa Oberta, Lliure i Neutral guifi.net
-# Mas l'Esperança, 08503 Gurb, Catalonia, Spain
+#    Copyright (C) 2011 Fundació Privada per a la Xarxa Oberta, Lliure i Neutral guifi.net
 #
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#    The full GNU General Public License is included in this distribution in
+#    the file called "COPYING".
 
 #######################
 # UCI related commands 
