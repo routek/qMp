9264a54c8d51fd84fb93de56d67709716f62fa6d
Simó Albert i Beltran <sim6@probeta.net>
2013-03-04 02:57:35 +0100

    Fix 'qmpinfo ipv4' in roaming mode, close #215.

--- packages/qmp-small-node/files/etc/qmp/qmpinfo.lua
+++ packages/qmp-small-node/files/etc/qmp/qmpinfo.lua
@@ -164,9 +164,15 @@ end
 
 function qmpinfo.get_ipv4()
 	local ipv4 = {}
-	local ipv4_raw = util.exec("ip -4 a | grep inet | awk '{print $2}' | awk -F/ '{print $1}' | grep -v 127.0.0.1")
+	local ipv4_raw = util.exec("ip -4 a | awk '/inet/{print $2}'")
 	for _,v in ipairs(util.split(ipv4_raw)) do
-		if #util.trim(v) > 1 then
+		local match = false
+		local i = 1
+		while i <= #ipv4 and not match do
+			match = string.match(util.trim(v),util.trim(ipv4[i]))
+			i = i + 1
+		end
+		if not match and #util.trim(v) > 1 then
 			table.insert(ipv4,util.trim(v))
 		end
 	end
