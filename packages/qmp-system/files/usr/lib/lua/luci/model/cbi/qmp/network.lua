ca1d0f28fe258c7e4214597b2034c785b5306dc3
Roger Pueyo Centelles <qmp@rogerpueyo.com>
2014-03-05 11:23:05 +0100

    Refurbishment of "Network" menu

--- packages/qmp-system/files/usr/lib/lua/luci/model/cbi/qmp/network.lua
+++ packages/qmp-system/files/usr/lib/lua/luci/model/cbi/qmp/network.lua
@@ -23,36 +23,42 @@ qmpinfo = require "qmpinfo"
 require("luci.sys")
 
 local http = require "luci.http"
-m = Map("qmp", "Quick Mesh Project")
+m = Map("qmp", "qMp network settings")
 
-eth_section = m:section(NamedSection, "interfaces", "qmp", "Interfaces", "Interfaces")
+eth_section = m:section(NamedSection, "interfaces", "qmp", translate("Network mode"), translate("Select the working mode of the wired network interfaces: <br/> · LAN mode is used to provide end-users connectivity and a DHCP will be enabled to assign IP addresses to the devices connecting.<br/> · WAN mode is used on interfaces connected to an Internet up-link or any other gateway connection.<br/> <br/>LAN and WAN modes are mutually exclusive. <strong>Do set an interface in both LAN and WAN modes.</strong>"))
 eth_section.addremove = False
+
+mesh_section = m:section(NamedSection, "interfaces", "qmp", translate("Mesh interfaces"), translate("Select the devices that will be used in the mesh network. It is recommended to select them all."))
+eth_section.addremove = False
+
+special_section = m:section(NamedSection, "interfaces", "qmp", translate("Special settings"), translate("Use this section to disable VLAN tagging in certain interfaces or to exclude them from qMp."))
+mesh_section.addremove = False
 
 -- Getting the physical (real) interfaces
 net_int = qmpinfo.get_devices().all
 
 -- Option: lan_devices
-lan = eth_section:option(MultiValue, "lan_devices", "LAN devices","These devices will be used for end-user connection (DHCP server)")
+lan = eth_section:option(MultiValue, "lan_devices", "LAN mode","Interfaces used to provide end-user connectivity (DHCP server)")
 local i,l
 for i,l in ipairs(net_int) do
 	lan:value(l,l)
 end
 
 -- Option wan_device
-wan = eth_section:option(MultiValue, "wan_devices", "WAN devices","These devices will be used for internet or any other gateway connection (DHCP client)")
+wan = eth_section:option(MultiValue, "wan_devices", "WAN mode","Interfaces connected to an Internet up-link or any other gateway (DHCP client)")
 for i,l in ipairs(net_int) do
 	wan:value(l,l)
 end
 
 -- Option mesh_devices
-mesh = eth_section:option(MultiValue, "mesh_devices", "MESH devices","These devices will be used for Mesh network")
+mesh = mesh_section:option(MultiValue, "mesh_devices", "MESH devices","Devices used for meshing (it is recommended to check them all)")
 for i,l in ipairs(net_int) do
         mesh:value(l,l)
 end
 
-no_vlan = eth_section:option(Value, "no_vlan_devices", translate("No VLAN devices"),translate("Devices we want to use without VLAN tagging (not recommended)"))
+no_vlan = special_section:option(Value, "no_vlan_devices", translate("VLAN-untagged devices"),translate("Devices that will not be used with VLAN tagging (it is recommended to leave it blank)"))
 
-ignore_devs = eth_section:option(Value, "ignore_devices", translate("Ignored devices"),translate("Devices are not taken into account for qMp autoconfiguration"))
+ignore_devs = special_section:option(Value, "ignore_devices", translate("Excluded devices"),translate("Devices that will not be used by qMp"))
 
 function m.on_commit(self,map)
 	http.redirect("/luci-static/resources/qmp/wait_long.html")
