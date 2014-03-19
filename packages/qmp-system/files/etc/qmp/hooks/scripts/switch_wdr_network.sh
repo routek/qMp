87cb9a9dbfbe33571cacba4ae02efcca3908e5d3
Pau Escrich <p4u@dabax.net>
2014-03-19 19:20:39 +0100

    Fix wdr4300 switch problem

--- packages/qmp-system/files/etc/qmp/hooks/scripts/switch_wdr_network.sh
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/switch_wdr_network.sh
@@ -19,9 +19,9 @@ done
 
 [ $iseth -eq 0 ] && {
 	echo "Device eth0 not configured, doing it..."
-	uci set qmp.interfaces.lan_devices="eth0.1"
+	uci set qmp.interfaces.lan_devices="eth0.2"
 	uci set qmp.interfaces.mesh_devices="eth0.12"
-	uci set qmp.interfaces.wan_devices="eth0.2"
+	uci set qmp.interfaces.wan_devices="eth0.3"
 	uci set qmp.interfaces.no_vlan_devices="eth0.12"
 	uci set qmp.interfaces.ignore_devices="eth0"
 	uci commit qmp
