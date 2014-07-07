87070dc08fbd763bdb1eb3881bda270fc651bbac
Pau Escrich <p4u@dabax.net>
2014-07-07 18:52:11 +0200

    Fix switch for wdr4300/wdr3600

--- packages/qmp-system/files/etc/qmp/hooks/scripts/switch_wdr.sh
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/switch_wdr.sh
@@ -30,21 +30,21 @@ uci add network switch_vlan > /dev/null
 uci add network switch_vlan > /dev/null
 
 uci set network.@switch[0]=switch
-uci set network.@switch[0].name=eth0
+uci set network.@switch[0].name=switch0
 uci set network.@switch[0].reset=1
 uci set network.@switch[0].enable_vlan=1
 uci set network.@switch_vlan[0]=switch_vlan
-uci set network.@switch_vlan[0].device=eth0
+uci set network.@switch_vlan[0].device=switch0
 uci set network.@switch_vlan[0].vlan=2
 uci set network.@switch_vlan[0].vid=2
 uci set network.@switch_vlan[0].ports="0t 2 3 4"
 uci set network.@switch_vlan[1]=switch_vlan
-uci set network.@switch_vlan[1].device=eth0
+uci set network.@switch_vlan[1].device=switch0
 uci set network.@switch_vlan[1].vlan=3
 uci set network.@switch_vlan[1].vid=3
 uci set network.@switch_vlan[1].ports="0t 1"
 uci set network.@switch_vlan[2]=switch_vlan
-uci set network.@switch_vlan[2].device=eth0
+uci set network.@switch_vlan[2].device=switch0
 uci set network.@switch_vlan[2].vlan=12
 uci set network.@switch_vlan[2].vid=12
 uci set network.@switch_vlan[2].ports="0t 5"
