4b71798555f5b54ea54150bd872f317b39425023
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-10-01 09:24:28 +0200

    Add hook for NanoStation M5 XW

---
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/nsm5-xw.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Stage = [birth|firstboot|anyboot|preconf|postconf]
+STAGE="$1"
+
+[ "$STAGE" == "birth" ] && {
+	echo "Configuring Ethernet switched Main and Secondary ports for NSM5-XW"
+	uci set qmp.interfaces.mesh_devices="wlan0"
+	uci set qmp.interfaces.lan_devices="eth0.1"
+	uci set qmp.interfaces.wan_devices="eth0.2"
+	uci set qmp.networks.disable_mesh_dhcp="0"
+	uci add qmp wireless
+	uci set qmp.@wireless[0]=wireless
+	uci set qmp.@wireless[0].mode=adhoc
+	uci set qmp.@wireless[0].device=wlan0
+	uci commit qmp
+}
