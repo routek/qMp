832ca1a56afac8f367bc96c0990b3a7084174314
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-12-21 11:43:27 +0100

    Add support for Nexx WT1520 devices

---
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/wt1520.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Stage = [birth|firstboot|anyboot|preconf|postconf]
+STAGE="$1"
+
+[ "$STAGE" == "birth" ] && {
+	echo "Configuring network and wifi for Nexx WT1520"
+	uci set qmp.interfaces.mesh_devices="wlan0"
+	uci set qmp.interfaces.lan_devices="eth0.1"
+	uci set qmp.interfaces.wan_devices="eth0.2"
+	uci set qmp.interfaces.ignore_devices="eth0"
+	uci set qmp.networks.disable_mesh_dhcp="0"
+	uci add qmp wireless
+	uci set qmp.@wireless[0]=wireless
+	uci set qmp.@wireless[0].mode=adhoc
+	uci set qmp.@wireless[0].device=wlan0
+	uci commit qmp
+}
---
+++ packages/qmp-system/files/etc/qmp/hooks/wt1520/wt1520.sh
@@ -0,0 +1,1 @@
+../scripts/wt1520.sh
