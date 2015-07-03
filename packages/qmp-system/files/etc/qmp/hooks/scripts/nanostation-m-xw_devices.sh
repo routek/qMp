293aa09c1b011d7d8beacc10ab02c60ecae551fb
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-07-03 23:58:04 +0200

    [qmp-system] Configure tagged VLAN 12 on the Secondary Ethernet port of NanoStations M5 XW and use it for mesh

--- packages/qmp-system/files/etc/qmp/hooks/scripts/nanostation-m-xw_devices.sh
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/nanostation-m-xw_devices.sh
@@ -4,8 +4,36 @@ STAGE="$1"
 
 [ "$STAGE" == "birth" ] && {
 	echo "Configuring Ethernet switched Main and Secondary ports for NSM5-XW"
+
+        uci -q delete network.@switch[0]
+        uci -q delete network.@switch_vlan[2]
+        uci -q delete network.@switch_vlan[1]
+        uci -q delete network.@switch_vlan[0]
+	uci commit network
+
+	uci add network switch
+	uci add network switch_vlan > /dev/null
+	uci add network switch_vlan > /dev/null
+
+	uci set network.@switch[0]=switch
+	uci set network.@switch[0].name=switch0
+	uci set network.@switch[0].reset=1
+	uci set network.@switch[0].enable_vlan=1
+	uci set network.@switch_vlan[0]=switch_vlan
+	uci set network.@switch_vlan[0].device=switch0
+	uci set network.@switch_vlan[0].vlan=1
+	uci set network.@switch_vlan[0].vid=1
+	uci set network.@switch_vlan[0].ports="0t 5"
+	uci set network.@switch_vlan[1]=switch_vlan
+	uci set network.@switch_vlan[1].device=switch0
+	uci set network.@switch_vlan[1].vlan=12
+	uci set network.@switch_vlan[1].vid=12
+	uci set network.@switch_vlan[1].ports="0t 1t"
+	uci commit network
+
 	uci set qmp.interfaces.lan_devices="eth0.1"
-	uci set qmp.interfaces.wan_devices="eth0.2"
+	uci set qmp.interfaces.mesh_devices="eth0.12"
+	uci set qmp.interfaces.no_lan_devices="eth0.12"
         uci set qmp.interfaces.ignore_devices="eth0"
 	uci add qmp wireless
 	uci set qmp.@wireless[0]=wireless
@@ -13,3 +41,4 @@ STAGE="$1"
 	uci set qmp.@wireless[0].device=wlan0
 	uci commit qmp
 }
+
