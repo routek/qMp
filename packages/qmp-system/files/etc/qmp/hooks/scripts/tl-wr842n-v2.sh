336d26484edb211d29ccc07dd350088afbb27165
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-08-17 14:15:18 +0200

    [qmp-system] Fix #361. Set eth0 as wan by default on TP-Link TL-WR842N[D]-v2

--- packages/qmp-system/files/etc/qmp/hooks/scripts/tl-wr842n-v2.sh
+++ packages/qmp-system/files/etc/qmp/hooks/scripts/tl-wr842n-v2.sh
@@ -5,6 +5,6 @@ STAGE="$1"
 [ "$STAGE" == "birth" ] && {
 	echo "Configuring network for TL-WR842N[D]-v2"
 	uci set qmp.interfaces.lan_devices="eth1"
-	uci set qmp.interfaces.lan_devices="eth0"
+	uci set qmp.interfaces.wan_devices="eth0"
 	uci commit qmp
 }
