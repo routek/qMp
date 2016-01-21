71a0f9db796cf6ccd5d45e435497bc0c2aee1dde
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2016-01-21 12:34:39 +0100

    [qmp-system] Set the default hostname to qMp

--- packages/qmp-system/files/usr/lib/lua/qmp_defaults.lua
+++ packages/qmp-system/files/usr/lib/lua/qmp_defaults.lua
@@ -4,11 +4,17 @@ local qmp_defaults = {}
 
 -- Default BSSID for Ad-Hoc wireless mode
 local bssid = "02:CA:FF:EE:BA:BE"
--- Default multicast rate (mcast_rate), in kb/s, for
-local mcast_rate = 18000
 -- Default country regulatory domain
 local country = "US"
+-- Default hostname
+local hostname = "qMp"
+-- Default multicast rate (mcast_rate), in kb/s, for
+local mcast_rate = 18000
 
+
+local function get_default_hostname()
+  return hostname
+end
 
 local function get_wireless_defaults()
   local t = {}
@@ -18,6 +24,7 @@ local function get_wireless_defaults()
   return (t)
 end
 
+qmp_defaults.get_default_hostname = get_default_hostname
 qmp_defaults.get_wireless_defaults = get_wireless_defaults
 
 return qmp_defaults
