f57315f405de04329f01860165fa66857e63b865
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2016-01-23 16:50:41 +0100

    [qmp-system] Add node section default values to qmp_defaults

--- packages/qmp-system/files/usr/lib/lua/qmp_defaults.lua
+++ packages/qmp-system/files/usr/lib/lua/qmp_defaults.lua
@@ -2,6 +2,21 @@
 
 local qmp_defaults = {}
 
+-- Node section
+-- Default device (node) name
+local node_id = "qMp"
+-- Default community id (none)
+local community_id = ''
+-- Default latitude
+local latitude = '0.0'
+-- Default longitude
+local longitude = '0.0'
+-- Default elevation
+local elevation = '5.0'
+-- Default contact
+local contact = 'your@email.qmp'
+
+-- Wireless section
 -- Default BSSID for Ad-Hoc wireless mode
 local bssid = "02:CA:FF:EE:BA:BE"
 -- Default country regulatory domain
@@ -10,11 +25,14 @@ local country = "US"
 local hostname = "qMp"
 -- Default multicast rate (mcast_rate), in kb/s, for
 local mcast_rate = 18000
+       
 
 
 local function get_default_hostname()
-  return hostname
+  return node_id
 end
+
+
 
 local function get_wireless_defaults()
   local t = {}
@@ -24,7 +42,21 @@ local function get_wireless_defaults()
   return (t)
 end
 
+
+
+local function get_node_defaults()
+  local t = {}
+  t["node_id"] = node_id
+  t["community_id"] = community_id
+  t["latitude"] = latitude
+  t["longitude"] = longitude
+  t["elevation"] = elevation
+  t["contact"] =  contact
+  return (t)
+end
+
 qmp_defaults.get_default_hostname = get_default_hostname
+qmp_defaults.get_node_defaults = get_node_defaults
 qmp_defaults.get_wireless_defaults = get_wireless_defaults
 
 return qmp_defaults
