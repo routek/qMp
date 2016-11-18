d27fbef7bd3d078d39af45bdc2d929583237e319
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2016-11-18 13:11:59 +0100

    [qmp_system] Add a function to qmp_io to get the contents of a [text] file, line by line

--- packages/qmp-system/files/usr/lib/lua/qmp/io.lua
+++ packages/qmp-system/files/usr/lib/lua/qmp/io.lua
@@ -5,9 +5,10 @@ local nixiofs = require("nixio.fs")
 local qmp_io = qmp_io or {}
 
 -- Local functions declaration
-local ls
 local is_file
-
+local ls
+local new_file
+local read_file
 
 
 -- Check if a file exists
@@ -16,17 +17,8 @@ function is_file (filename)
 end
 
 
-
--- Create a new (empty) file
-function new_file (filename)
-  nixiofs.writefile(filename, '')
-  return is_file(filename)
-end
-
-
 -- List a directory
 function ls (dirname)
-
   local dircontent = {}
 
   local i, t, popen = 0, {}, io.popen
@@ -36,16 +28,36 @@ function ls (dirname)
   end
 
   return dircontent
-
 end
 
 
+-- Create a new (empty) file
+function new_file (filename)
+  nixiofs.writefile(filename, '')
+  return is_file(filename)
+end
 
+
+-- Read a file and return its content, line per line, in a table
+function read_file (filename)
+  -- Check if the file exists
+  if is_file(filename) then
+    local lines = {}
+
+    for line in io.lines(filename) do
+      table.insert(lines, line)
+    end
+
+    return lines
+  end
+  return
+end
 
 
 qmp_io.is_file = is_file
-qmp_io.new_file = new_file
 qmp_io.ls = ls
+qmp_io.new_file = new_file
+qmp_io.read_file = read_file
 
 
 return qmp_io
